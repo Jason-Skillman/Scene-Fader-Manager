01772cedf84d82489d5faf055d1b718417948c26
JasonSkillman <jason_skillman@outlook.com>
2020-09-02 13:57:26 -0400

    Created fade out animation

---
+++ Assets/Scene-Loader-Manager/Scripts/SceneLoaderManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderManager : MonoBehaviour {
+
+	public static SceneLoaderManager Instance { get; private set; }
+
+	private Animator animator;
+
+	private Action fadeFinished;
+
+	void Awake() {
+		if(Instance == null) Instance = this;
+		else Destroy(gameObject);
+		DontDestroyOnLoad(gameObject);
+
+		animator = GetComponent<Animator>();
+	}
+
+	void Update() {
+	}
+
+	public void LoadScene(string sceneName) {
+		fadeFinished = () => {
+			//StartCoroutine(Load(sceneName));
+			Debug.Log("done");
+		};
+		animator.SetTrigger("fadeOut");
+	}
+
+	
+
+	private IEnumerator Load(string sceneName) {
+		yield return null;
+
+		//Begin to load the Scene you specify
+		AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+		
+		//Don't let the Scene activate until you allow it to
+		asyncOperation.allowSceneActivation = false;
+		Debug.Log("Pro :" + asyncOperation.progress);
+		
+		//When the load is still in progress, output the Text and progress bar
+		while(!asyncOperation.isDone) {
+			//Output the current progress
+			//m_Text.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
+			Debug.Log("Pro :" + (asyncOperation.progress * 100) + "%");
+
+			// Check if the load has finished
+			if(asyncOperation.progress >= 0.9f) {
+				//Change the Text to show the Scene is ready
+				//m_Text.text = "Press the space bar to continue";
+				Debug.Log("Press the space bar to continue");
+				
+				//Wait to you press the space key to activate the Scene
+				if(Input.GetKeyDown(KeyCode.Space))
+					//Activate the Scene
+					asyncOperation.allowSceneActivation = true;
+			}
+
+			yield return null;
+		}
+	}
+	
+	/// <summary>
+	/// Called externally by the animation
+	/// </summary>
+	private void AnimationFinished() {
+		fadeFinished();
+	}
+
+}
