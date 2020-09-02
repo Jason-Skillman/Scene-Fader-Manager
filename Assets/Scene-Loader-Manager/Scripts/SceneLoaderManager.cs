c0f4e185d7e975e7ac94021102f88cfbd8121f56
JasonSkillman <jason_skillman@outlook.com>
2020-09-02 16:02:01 -0400

    Added min wait timer

--- Assets/Scene-Loader-Manager/Scripts/SceneLoaderManager.cs
+++ Assets/Scene-Loader-Manager/Scripts/SceneLoaderManager.cs
@@ -9,11 +9,15 @@ public class SceneLoaderManager : MonoBehaviour {
 
 	public static SceneLoaderManager Instance { get; private set; }
 
-	public GameObject prefabLoadingScreen;
+	/// <summary>
+	/// The minimum time the loading screen will wait for in seconds
+	/// </summary>
+	public float minWaitTime = 3.0f;
 
 	[Header("References")]
 	public GameObject canvas;
-	
+	public GameObject prefabLoadingScreen;
+
 	private Animator animator;
 	private CanvasGroup canvasGroup;
 
@@ -21,6 +25,8 @@ public class SceneLoaderManager : MonoBehaviour {
 	private GameObject loadingScreenRef;
 	
 	public bool IsOn { get; private set; }
+	
+	public int Progress { get; private set; }
 
 	void Awake() {
 		if(Instance == null) Instance = this;
@@ -68,32 +74,36 @@ public class SceneLoaderManager : MonoBehaviour {
 	private IEnumerator Load(string sceneName) {
 		yield return null;
 
+		float time = 0;
+
 		//Begin to load the Scene you specify
 		AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
 		
 		//Don't let the Scene activate until you allow it to
 		asyncOperation.allowSceneActivation = false;
-		Debug.Log("Pro :" + asyncOperation.progress);
 		
 		//When the load is still in progress, output the Text and progress bar
 		while(!asyncOperation.isDone) {
+			//Keep track of how much time has gone by
+			time += Time.deltaTime;
+			//Debug.Log("t: " + time);
+			
 			//Output the current progress
-			//m_Text.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
-			Debug.Log("Pro :" + (asyncOperation.progress * 100) + "%");
+			Progress = (int)(asyncOperation.progress * 100);
 
 			// Check if the load has finished
 			if(asyncOperation.progress >= 0.9f) {
-				//Change the Text to show the Scene is ready
-				//m_Text.text = "Press the space bar to continue";
-				Debug.Log("Press the space bar to continue");
-				
-				//Wait to you press the space key to activate the Scene
-				if(Input.GetKeyDown(KeyCode.Space)) {
-					//Activate the Scene
-					asyncOperation.allowSceneActivation = true;
-
-					FadeOut();
+				//Block final load if minimum time has not been reached
+				if(time < minWaitTime) {
+					yield return null;
+					continue;
 				}
+				
+				//Finished
+				
+				asyncOperation.allowSceneActivation = true;
+
+				FadeOut();
 				
 			}
 
